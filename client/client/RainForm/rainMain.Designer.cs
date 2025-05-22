946a3e872b1cad396dd398b9c77b7db57187b32b
LEE CHAE EUN <chaeeun8837@kw.ac.kr>
2025-05-22 16:13:42 +0900

    산성비미완

---
+++ client/client/RainForm/rainMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace client.RainForm
+{
+    partial class rainMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rainMain));
+            this.inputTxt = new Guna.UI2.WinForms.Guna2TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lbScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTxt
+            // 
+            this.inputTxt.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.inputTxt.DefaultText = "";
+            this.inputTxt.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.inputTxt.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.inputTxt.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.inputTxt.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.inputTxt.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.inputTxt.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.inputTxt.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.inputTxt.Location = new System.Drawing.Point(100, 369);
+            this.inputTxt.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.inputTxt.Name = "inputTxt";
+            this.inputTxt.PlaceholderText = "";
+            this.inputTxt.SelectedText = "";
+            this.inputTxt.Size = new System.Drawing.Size(160, 35);
+            this.inputTxt.TabIndex = 0;
+            this.inputTxt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputTxt_KeyDown);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lbScore
+            // 
+            this.lbScore.AutoSize = true;
+            this.lbScore.Location = new System.Drawing.Point(28, 28);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(89, 26);
+            this.lbScore.TabIndex = 1;
+            this.lbScore.Text = "점수: 0";
+            // 
+            // rainMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(374, 429);
+            this.Controls.Add(this.lbScore);
+            this.Controls.Add(this.inputTxt);
+            this.Font = new System.Drawing.Font("휴먼옛체", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Name = "rainMain";
+            this.Text = "산성비";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox inputTxt;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lbScore;
+    }
+}
--- client/client/menuControl/미니게임.cs
+++ client/client/menuControl/미니게임.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using client.RainForm;
 
 namespace client.menuControl
 {
     public partial class 미니게임 : UserControl
     {
+        rainMain main;
+
         public 미니게임()
         {
             InitializeComponent();
+        }
+
+        private void rainBtn_Click(object sender, EventArgs e)
+        {
+            main=new rainMain();
+            main.Show();
         }
     }
 }
