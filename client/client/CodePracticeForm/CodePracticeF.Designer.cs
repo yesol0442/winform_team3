05f8d1740c741067462c3fec9c7a4977237e3cce
CHEON JIYEON <milk0618@kw.ac.kr>
2025-05-23 13:41:50 +0900

    그뭐냐 그거 그 포커스

--- client/client/CodePracticeForm/CodePracticeF.Designer.cs
+++ client/client/CodePracticeForm/CodePracticeF.Designer.cs
@@ -29,10 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
-            this.내림 = new System.Windows.Forms.Button();
-            this.나가기 = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.정확도TB = new System.Windows.Forms.TextBox();
@@ -40,43 +36,11 @@
             this.label2 = new System.Windows.Forms.Label();
             this.타수TB = new System.Windows.Forms.TextBox();
             this.코드연습panel = new System.Windows.Forms.TableLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.나가기 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // guna2BorderlessForm1
-            // 
-            this.guna2BorderlessForm1.ContainerControl = this;
-            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
-            this.guna2BorderlessForm1.TransparentWhileDrag = true;
-            // 
-            // 내림
-            // 
-            this.내림.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.내림.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.내림.Location = new System.Drawing.Point(1365, 12);
-            this.내림.Name = "내림";
-            this.내림.Size = new System.Drawing.Size(33, 27);
-            this.내림.TabIndex = 3;
-            this.내림.Text = "_";
-            this.내림.UseVisualStyleBackColor = true;
-            this.내림.Click += new System.EventHandler(this.내림_Click);
-            // 
-            // 나가기
-            // 
-            this.나가기.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.나가기.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.나가기.Location = new System.Drawing.Point(1400, 12);
-            this.나가기.Name = "나가기";
-            this.나가기.Size = new System.Drawing.Size(33, 27);
-            this.나가기.TabIndex = 2;
-            this.나가기.Text = "X";
-            this.나가기.UseVisualStyleBackColor = true;
-            this.나가기.Click += new System.EventHandler(this.나가기_Click);
-            // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // tableLayoutPanel1
             // 
@@ -89,12 +53,12 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.71238F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.코드연습panel, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 45);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 87);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1421, 592);
-            this.tableLayoutPanel1.TabIndex = 4;
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1258, 523);
+            this.tableLayoutPanel1.TabIndex = 5;
             // 
             // tableLayoutPanel2
             // 
@@ -110,7 +74,7 @@
             this.tableLayoutPanel2.Controls.Add(this.label1, 1, 1);
             this.tableLayoutPanel2.Controls.Add(this.label2, 1, 4);
             this.tableLayoutPanel2.Controls.Add(this.타수TB, 1, 2);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(1257, 3);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(1113, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 7;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.75538F));
@@ -120,7 +84,7 @@
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2.731221F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.377688F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.73726F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(161, 586);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(142, 517);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // 정확도TB
@@ -128,10 +92,10 @@
             this.정확도TB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.정확도TB.Location = new System.Drawing.Point(19, 218);
+            this.정확도TB.Location = new System.Drawing.Point(17, 193);
             this.정확도TB.Name = "정확도TB";
             this.정확도TB.ReadOnly = true;
-            this.정확도TB.Size = new System.Drawing.Size(122, 28);
+            this.정확도TB.Size = new System.Drawing.Size(107, 28);
             this.정확도TB.TabIndex = 3;
             this.정확도TB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -139,9 +103,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("휴먼옛체", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(19, 86);
+            this.label1.Location = new System.Drawing.Point(17, 76);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.Size = new System.Drawing.Size(54, 14);
             this.label1.TabIndex = 0;
             this.label1.Text = "타수";
             // 
@@ -149,9 +113,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("휴먼옛체", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(19, 199);
+            this.label2.Location = new System.Drawing.Point(17, 176);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.Size = new System.Drawing.Size(76, 14);
             this.label2.TabIndex = 1;
             this.label2.Text = "정확도";
             // 
@@ -160,10 +124,10 @@
             this.타수TB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.타수TB.Location = new System.Drawing.Point(19, 105);
+            this.타수TB.Location = new System.Drawing.Point(17, 93);
             this.타수TB.Name = "타수TB";
             this.타수TB.ReadOnly = true;
-            this.타수TB.Size = new System.Drawing.Size(122, 28);
+            this.타수TB.Size = new System.Drawing.Size(107, 28);
             this.타수TB.TabIndex = 2;
             this.타수TB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -190,18 +154,32 @@
             this.코드연습panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
             this.코드연습panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
             this.코드연습panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
-            this.코드연습panel.Size = new System.Drawing.Size(1248, 586);
+            this.코드연습panel.Size = new System.Drawing.Size(1104, 517);
             this.코드연습panel.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // 나가기
+            // 
+            this.나가기.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.나가기.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.나가기.Location = new System.Drawing.Point(1234, 24);
+            this.나가기.Name = "나가기";
+            this.나가기.Size = new System.Drawing.Size(33, 27);
+            this.나가기.TabIndex = 6;
+            this.나가기.Text = "X";
+            this.나가기.UseVisualStyleBackColor = true;
+            this.나가기.Click += new System.EventHandler(this.나가기_Click);
             // 
             // CodePracticeF
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1445, 754);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.내림);
+            this.ClientSize = new System.Drawing.Size(1282, 707);
             this.Controls.Add(this.나가기);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "CodePracticeF";
             this.Text = "CodePracticeF";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CodePracticeF_FormClosed);
@@ -216,16 +194,14 @@
 
         #endregion
 
-        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
-        private System.Windows.Forms.Button 내림;
-        private System.Windows.Forms.Button 나가기;
-        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox 정확도TB;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox 정확도TB;
         private System.Windows.Forms.TextBox 타수TB;
         private System.Windows.Forms.TableLayoutPanel 코드연습panel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button 나가기;
     }
 }
