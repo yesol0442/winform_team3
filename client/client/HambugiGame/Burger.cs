24f20aa9631e49c11ab8da333e0e1d140dc57012
CHEON JIYEON <milk0618@kw.ac.kr>
2025-06-17 13:06:24 +0900

    햄부기게임클래스

---
+++ client/client/HambugiGame/Burger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.HambugiGame
+{
+    public class Burger
+    {
+        public List<Ingredient> Layers { get; } = new List<Ingredient>();
+        public void AddIngredient(Ingredient ing) => Layers.Add(ing);
+    }
+}
---
+++ client/client/HambugiGame/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.HambugiGame
+{
+    public class Order
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public List<IngredientType> Recipe { get; }
+        public int BasePrice { get; }          // 주문표에 적힌 가격
+        public string Comment { get; }
+        public Order(List<IngredientType> recipe, int price, string comment)
+            => (Recipe, BasePrice, Comment) = (recipe, price, comment);
+    }
+}
