537b7961feb01ae5cf9aec9ed8d1dc7e7480e35e
CHEON JIYEON <milk0618@kw.ac.kr>
2025-06-17 15:16:27 +0900

    햄부기추가

---
+++ client/client/HambugiGame/OrderGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client.HambugiGame
+{
+    public class OrderGenerator
+    {
+        private readonly Timer _timer;
+        private readonly List<Order> _orders = new List<Order>();
+        private readonly Random _rand = new Random();
+
+        public event Action<Order> OnOrderCreated;
+
+        public OrderGenerator()
+        {
+            _timer = new Timer { Interval = _rand.Next(1000, 3000) };
+            _timer.Tick += (_, _) =>
+            {
+                if (_orders.Count >= 5) return;
+
+                var order = MakeRandomOrder();
+                _orders.Add(order);
+                OnOrderCreated?.Invoke(order);
+
+                // 다음 간격 재설정
+                _timer.Interval = _rand.Next(1000, 3000);
+            };
+        }
+
+        public void Start() => _timer.Start();
+        private Order MakeRandomOrder()
+        {
+            // 예시: 패티 1장 기본 햄버거
+            var recipe = new List<IngredientType>
+        {
+            IngredientType.BunBottom,
+            IngredientType.PattyMedium,
+            IngredientType.Tomato,
+            IngredientType.Lettuce,
+            IngredientType.BunTop
+        };
+            return new Order(recipe, 500, "기본으로 주세요!");
+        }
+    }
+}
---
+++ client/client/HambugiGame/ScoringEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.HambugiGame
+{
+    public class ScoringEngine
+    {
+        public (int earned, string comment) Evaluate(Order order, Burger cooked)
+        {
+            // 1. 조립 여부 (맨 위·아래가 빵인지 확인)
+            if (cooked.Layers.FirstOrDefault()?.Type != IngredientType.BunBottom ||
+                cooked.Layers.LastOrDefault()?.Type != IngredientType.BunTop)
+                return (0, "음식이 장난이야?");
+
+            // 2. 패티 익힘
+            var patty = cooked.Layers.FirstOrDefault(x => x.Type.ToString().Contains("Patty"));
+            if (patty != null && patty.CookState == CookState.Raw)
+                return (-5000, "엑 하나도 안 익었잖아! 병원비 줘!");
+
+            // 3. 순서·구성 비교
+            bool perfect = cooked.Layers.Select(l => l.Type).SequenceEqual(order.Recipe);
+            if (perfect) return (order.BasePrice, "완벽해요!");
+
+            // 누락·순서 오류 → 50% 삭감
+            return ((int)(order.BasePrice * 0.5), "뭔가 빠졌거나 순서가 이상해요");
+        }
+    }
+}
