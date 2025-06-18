8297b143f5d426ee9ffc07ff035160fa6a1acf9e
LEE CHAE EUN <chaeeun8837@kw.ac.kr>
2025-06-18 22:36:23 +0900

    게임방법통일

--- client/client/FindDifferForm/FindTutorial.cs
+++ client/client/FindDifferForm/FindTutorial.cs
@@ -24,11 +24,6 @@ namespace client.FindDifferForm
 
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.Text = "게임 방법";
-
-            label2.Text= "1. 코드에 숨겨진 오류를 찾아 마우스로 클릭하세요.\n\n" +
-                "2. 오류를 더 많이 맞히는 사람이 승리합니다.\n\n" +
-                "3. 정답을 클릭하면 색으로 표시됩니다.\n\n" +
-                "4. 상대방의 정답도 실시간으로 확인할 수 있습니다.";
         }
 
        
