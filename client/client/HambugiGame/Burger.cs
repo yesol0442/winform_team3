9e35700ef609fcdcacc296ee0c479248aa343a49
CHEON JIYEON <milk0618@kw.ac.kr>
2025-06-19 17:43:41 +0900

    벅어

--- client/client/HambugiGame/Burger.cs
+++ client/client/HambugiGame/Burger.cs
@@ -8,7 +8,7 @@ namespace client.HambugiGame
 {
     public class Burger
     {
-        public List<Ingredient> Layers { get; } = new List<Ingredient>();
+        public List<Ingredient> Layers { get; set; } = new List<Ingredient>();
         public void AddIngredient(Ingredient ing) => Layers.Add(ing);
 
         public Ingredient FindFirstIngredient(IngredientType t)
